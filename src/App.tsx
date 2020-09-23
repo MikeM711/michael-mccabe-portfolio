15b33acf2bb052810a85e9b1ffd4a023f6eeca1a
Michael <michaelmccabe711@gmail.com>
2020-09-22 22:39:12 -0400

    added card for the main nav, works on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import profileImage from './img/michael-pic.png';
-import './App.css';
+import React, {useState, useEffect} from "react";
+
+import NavMain from "./nav-items/nav-main";
+import "./App.css";
 
 function App() {
+
+  const [windowSize, setWindowSize] = useState(window.innerWidth)
+  const isMobile = windowSize < 1042;
+
+  React.useEffect(() => {
+    // https://blog.logrocket.com/developing-responsive-layouts-with-react-hooks/
+    /* Inside of a "useEffect" hook add an event listener that updates
+       the "width" state variable when the window size changes */
+    window.addEventListener("resize", () => setWindowSize(window.innerWidth));
+
+    /* passing an empty array as the dependencies of the effect will cause this
+       effect to only run when the component mounts, and not each time it updates.
+       We only want the listener to be added once */
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Michael McCabe - Website</h1>
-      <h2>Software Engineer</h2>
-      <img src={profileImage} alt="profile-image"/>
+    <div id="App" className="container">
+      {isMobile ? (
+        <div>Hello mobile</div>
+      ) : (
+        <div id="nav-items">
+          <NavMain />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/nav-items/nav-main.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import profileImage from "../img/michael-pic.png";
+
+function NavMain() {
+  return (
+    <div className="row nav-main valign-wrapper">
+      <div className="col ">
+        <div className="card horizontal">
+          <div className="card-stacked">
+            <div className="card-content">
+              <h3>Michael McCabe</h3>
+              <p className="my-status">Software Developer Master's Student</p>
+              <p className="my-status">at Boston University</p>
+            </div>
+          </div>
+          <div className="card-image">
+            <img
+              src={profileImage}
+              alt="profile-image"
+              style={{ filter: "brightness(0%)" }}
+            />
+          </div>
+        </div>
+      </div>
+      {/* <img src={profileImage} alt="profile-image"/> */}
+    </div>
+  );
+}
+
+export default NavMain;
