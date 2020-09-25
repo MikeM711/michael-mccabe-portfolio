e5086ba84ab508cd6c00bbfdf97c26c73a44fb59
Michael <michaelmccabe711@gmail.com>
2020-09-25 19:02:28 -0400

    stylized navbar

--- src/nav-items/navbar.tsx
+++ src/nav-items/navbar.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect } from "react";
 import M from "materialize-css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faBolt, faChartBar, faCode, faHome, faLayerGroup, faToolbox } from '@fortawesome/free-solid-svg-icons';
+
+import './navbar.css';
 
 const NavBar: React.FC = () => {
   useEffect(() => {
@@ -12,26 +16,43 @@ const NavBar: React.FC = () => {
       <div className="row">
         <div className="col s12">
           <ul className="tabs">
-            <li className="tab col offset-l2 l2">
-              <a className="active" href="#test1">
-                <i className="material-icons">search</i>
-              </a>
-            </li>
+
             <li className="tab col l2">
-              <a href="#test2">
-                <i className="material-icons">view_module</i>
+              <a className="active" href="#home">
+                <FontAwesomeIcon className="home fa-3x" icon={faHome} />
               </a>
             </li>
+
             <li className="tab col l2">
-              <a href="#test3">
-                <i className="material-icons">refresh</i>
+              <a href="#experience">
+                <FontAwesomeIcon className="experience fa-3x" icon={faToolbox} />
               </a>
             </li>
+
             <li className="tab col l2">
-              <a href="#test4">
-                <i className="material-icons">more_vert</i>
+              <a href="#open-source">
+                <FontAwesomeIcon className="open-source fa-3x" icon={faCode} />
               </a>
             </li>
+
+            <li className="tab col l2">
+              <a href="#projects">
+                <FontAwesomeIcon className="projects fa-3x" icon={faLayerGroup} />
+              </a>
+            </li>
+
+            <li className="tab col l2">
+              <a href="#presentations">
+                <FontAwesomeIcon className="presentations fa-3x" icon={faChartBar} />
+              </a>
+            </li>
+
+            <li className="tab col l2">
+              <a href="#current">
+              <FontAwesomeIcon className="current fa-3x" icon={faBolt} />
+              </a>
+            </li>
+
           </ul>
         </div>
       </div>
