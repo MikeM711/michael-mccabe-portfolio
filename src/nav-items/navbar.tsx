1ac53bb89ca042e46929917e688563bb0d16d732
Michael <michaelmccabe711@gmail.com>
2020-09-25 22:36:45 -0400

    navbar descriptors

--- src/nav-items/navbar.tsx
+++ src/nav-items/navbar.tsx
@@ -7,48 +7,55 @@ import './navbar.css';
 
 const NavBar: React.FC = () => {
   useEffect(() => {
+    // tabs initialization
     const elem = document.querySelectorAll(".tabs");
     const options = {};
     const instance = M.Tabs.init(elem, options);
   }, []);
+
   return (
     <div className="site-navbar">
       <div className="row">
         <div className="col s12">
           <ul className="tabs">
-
             <li className="tab col l2">
-              <a className="active" href="#home">
+              <a className="active home" href="#home">
+                <span>Home</span>
                 <FontAwesomeIcon className="home fa-3x" icon={faHome} />
               </a>
             </li>
 
             <li className="tab col l2">
               <a href="#experience">
+              <span>Experience</span>
                 <FontAwesomeIcon className="experience fa-3x" icon={faToolbox} />
               </a>
             </li>
 
             <li className="tab col l2">
               <a href="#open-source">
+              <span>Open Source</span>
                 <FontAwesomeIcon className="open-source fa-3x" icon={faCode} />
               </a>
             </li>
 
             <li className="tab col l2">
               <a href="#projects">
+              <span>Projects</span>
                 <FontAwesomeIcon className="projects fa-3x" icon={faLayerGroup} />
               </a>
             </li>
 
             <li className="tab col l2">
               <a href="#presentations">
+                <span>Presentations</span>
                 <FontAwesomeIcon className="presentations fa-3x" icon={faChartBar} />
               </a>
             </li>
 
             <li className="tab col l2">
               <a href="#current">
+              <span>Current</span>
               <FontAwesomeIcon className="current fa-3x" icon={faBolt} />
               </a>
             </li>
