b726cddf9a5986df9b606e0ab90a71bdd1ab6a98
Michael <michaelmccabe711@gmail.com>
2020-10-25 16:07:28 -0400

    Moving all universal css into App.css, flexing out “experience” item

--- src/nav-items/nav-experience.tsx
+++ src/nav-items/nav-experience.tsx
@@ -1,27 +1,78 @@
 import React from "react";
 
+import "./nav-experience.css";
+
 const NavExperience: React.FC = () => {
   return (
     <>
       <div className="card-stacked nav-experience">
         <div className="card-content">
           <h3 className="card-title">Experience</h3>
-          <hr/>
-          <p>long string</p>
-          <p>long string</p>
-          <p>long string</p>
-          <p>long string</p>
-          <p>long string</p>
-          <p>long string</p>
-          <p>long string</p>
-          <p>long string</p>
-          <p>long string</p>
-          <p>long string</p>
-          <p>long string</p>
-          <p>long string</p>
-          <p>long string</p>
-          <p>long string</p>
-          <p>long string</p>
+          <hr />
+          <p>
+            Hi, my name is Michael McCabe and I am a Software Developer Master's
+            Student, Engineer and Open Source contributor.
+          </p>
+          <p>
+            During my Mechanical Engineering career, I have spent a considerable
+            amount of time working with both JavaScript and Python, and
+            frameworks such as React and Node.js, in my spare time. At work, I
+            have used these tools to both automate and maximize productivity. In
+            particular, I have developed a full-stack production planner web
+            application that solved many production-related business problems
+            for my employer.
+          </p>
+          <p>
+            After two years of being a Software Developer hobbyist, I have come
+            to the conclusion that Software Development is the correct career
+            choice. I have been enrolled in Boston University's Software
+            Development Master program (online) ever since.
+          </p>
+          <p>Expected graduation: August 2021</p>
+          <hr />
+          <h4 className="card-sub-title">Courses Completed</h4>
+          <p>
+            <b>Server-Side Web Development:</b> Hands-on web application
+            development using Node.js, MongoDB (NoSQL), JavaScript, Express
+            along with PHP/MySQL experience.
+          </p>
+          <p>
+            <b>Database Design and Implementation for Business:</b> A deep dive
+            into relational databases, SQL, day-to-day transactional databases,
+            data warehouses, distributed databases and cloud services.
+          </p>
+          <p>
+            <b>Information Structures with Python:</b> A focus on a strong
+            foundation in Python programming.
+          </p>
+          <hr />
+          <h4 className="card-sub-title skills-sub-title">Skills</h4>
+          <div className="row skill-listing center">
+            <div className="col l3">
+              <p className="center-align">JavaScript</p>
+            </div>
+            <div className="col l3">
+              <p>Python</p>
+            </div>
+            <div className="col l3">
+              <p>Node.js</p>
+            </div>
+            <div className="col l3">
+              <p>Express</p>
+            </div>
+            <div className="col l3">
+              <p>React/Redux</p>
+            </div>
+            <div className="col l3">
+              <p>MongoDB</p>
+            </div>
+            <div className="col l3">
+              <p>PostgreSQL</p>
+            </div>
+            <div className="col l3">
+              <p>HTML/CSS</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/project-data/project-data.tsx
+++ src/project-data/project-data.tsx
@@ -7,8 +7,8 @@ const projectData = [
         "title": "ParseIt",
         "repoName": "ParseIt",
         "description": ["A full-stack web tool that allows non-technical people the ability to extract information, or perform data science, for large amounts of PDF documents or complex text data.",
-        "ParseIt comes with 21 simple, open-ended modules that can either add, remove, replace, split or save text. The purpose of ParseIt is to \"stack\" these modules on top of each other, and have your input funneled through each module",
-        "The tool incorporates text-preview features that use text highlighting to let the user know exactly how they are parsing their data before they commit to a parsing action."
+        "ParseIt comes with 21 simple, open-ended modules that can either add, remove, replace, split or save text. The purpose of ParseIt is to \"stack\" these modules on top of each other, and have your input funneled through each module. As modules are stacked, one can solve very challenging and unique parsing problems.",
+        "The tool incorporates text-preview features that use text highlighting as a guide to let the user know exactly how they are parsing their data, before they commit to a parsing action."
         ],
         "projectImage": parseit,
         "alt": "parseIt"
@@ -26,7 +26,7 @@ const projectData = [
         "title": "Handwriting Recognition",
         "repoName": "Deep-Learning-Handwriting-Recognition",
         "description": ["A web tool that utilizes pre-trained deep learning models to predict users' handwriting using a resizable drawing canvas.", 
-        "The full A.I. network utilizes a jury of 5 convolutional models to create a singular prediction.", 
+        "The full A.I. network utilizes a \"jury\" of 5 convolutional models that takes the aggregate of all results to create a singular prediction.", 
         "See my Python Notebook inside the Code link for more detail on how these models were trained."
         ],
         "projectImage": handwritingRecognition,
