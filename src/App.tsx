a034c45d5354bd7b572d8b14e031b880c3c62259
Michael <michaelmccabe711@gmail.com>
2020-10-29 19:38:35 -0400

    Added contact row images and resume

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,12 @@ import HomeMobile from './mobile-components/home-mobile';
 import "./App.css";
 import "./App-mobile.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import gitIcon from './img/icons/GitHub-Mark-Light-120px-plus.png';
+import liIcon from './img/icons/linkedin-icon.png';
+import { faInbox, faFileWord } from '@fortawesome/free-solid-svg-icons';
+import Pdf from './docs/resume.pdf';
+
 const App: React.FC = () => {
   const [windowSize, setWindowSize] = useState<number>(window.innerWidth);
   const [activeComponent, setActiveComponent] = useState<string>("home");
@@ -121,8 +127,32 @@ const App: React.FC = () => {
                     </div>
                   )
                 })}
-                
               </div>
+            
+              <div className="row contact-information">
+                <a href="mailto:michaelmccabe711@gmail.com" target="_blank">
+                  <FontAwesomeIcon className="fa fa-inbox fa-3x" icon={faInbox} />
+                </a>
+
+                <span className="dot-separator"></span>
+
+                <a href="https://github.com/MikeM711" target="_blank">
+                  <img className="git-icon" src={gitIcon} alt="git-icon"/>
+                </a>
+
+                <span className="dot-separator"></span>
+
+                <a href="https://www.linkedin.com/in/michaelmccabe711/" target="_blank">
+                  <img className="li-icon" src={liIcon} alt="li-icon"/>
+                </a>
+
+                <span className="dot-separator"></span>
+
+                <a href={Pdf} target="_blank">
+                  <FontAwesomeIcon className="fa fa-resume fa-3x" icon={faFileWord} />
+                </a>
+              </div>
+
             </div>
           </div>
         </div>
